553c9c183af1d475f35f63ca4c2cdeb43700b2af
Shane Moodie <shanepmoodie96@gmail.com>
2017-10-09 11:13:15 -0400

    Initial commit 1009

--- Week2Day4OOP/Cat.cs
+++ Week2Day4OOP/Cat.cs
@@ -9,10 +9,13 @@ namespace Week2Day4OOP
     class Cat
     {
         //STATES
-        //FIELDS
+        //FIELDS - usually private state, characteristics of class that WE build
         private string name;
+        private int age;
+        private string furColor;
+        private bool IsHungry = true;
          
-        //PROPERTIES
+        //PROPERTIES - usually public state, characteristics fo class that allow us to interact 
         public string Name
         {
             get { return this.name; }
@@ -25,11 +28,42 @@ namespace Week2Day4OOP
 
 
 
-        //CONSTRUCTORS
+        //CONSTRUCTORS - create new instances
         public Cat()
         {
             //default constructor b/c it takes no parameters in parenthesis
+            //new constructor w/ same name but diff. parameters (see below) = overloaded constructor
+
         }
+
+        //overloaded constructor, allows to define parameters right away
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        //Methods
+        public void Eat()
+        {
+            if(IsHungry == true)
+            {
+                IsHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry? " + IsHungry);
+        }
+
+        public void WorkOut()
+        {
+            if(IsHungry == false)
+            {
+                IsHungry = true;
+            }
+
+            Console.WriteLine("The cat has worked out, hungry now? " + IsHungry);
+        }
+            
 
 
     }
--- Week2Day4OOP/Program.cs
+++ Week2Day4OOP/Program.cs
@@ -9,7 +9,19 @@ namespace Week2Day4OOP
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            Cat mittens = new Cat();
+            mittens.Name = "Mittens";
+
+            Console.WriteLine(mittens.Name); //Prints out "Mittens"
+
+            Cat mumbo = new Cat("Mumbo", 3, "grey and white");
+
+            Console.WriteLine(mumbo.Name); //Prints out Mumbo.
+
+            mittens.Eat();
+
+            mittens.WorkOut();
         }
     }
 }
